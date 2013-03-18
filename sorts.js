b664772c539d72bd3d93a93b3beb826c9318f1de
jbranchaud <jbranchaud@gmail.com>
2013-03-18 10:41:25 -0500

    Adding two assert functions and then using them to verify the results of my functions.

--- sorts.js
+++ sorts.js
@@ -1,3 +1,46 @@
+/*
+ * sorts.js - a variety of sorting algorithms and other functions
+ */
+
+
+// assert - this function will wrap the above assert method and
+// remove the need to pass in a string, instead, the expression
+// that is given will be used as the message passed into assert.
+function assert(expression) {
+    assert(expression, '');
+}
+
+// assert - this function will add support for assertions. Two items
+// can be passed in and their equality will be tested. If they are
+// evaluated to be equal, then the function will silently return,
+// otherwise an error will be thrown using the message given in the
+// second argument.
+//
+// This function will add support for assertions so that I
+// can actually do quick inline testing of my code to see that certain
+// properties hold during execution and that the outputs of functions
+// are what I expect.
+//
+// Example:
+// assert(4<5, "comparing 4 and 5");
+// > undefined
+// assert(4>5, "comparing 4 and 5");
+// > Error: Assertion Violated: comparing 4 and 5
+function assert(expression, message) {
+    if(!expression) {
+        var errorMessage = '';
+        if(message) {
+            errorMessage = "Assertion Violated: " + message;
+        }
+        else {
+            errorMessage = "Assertion Violated";
+        }
+
+        throw new Error(errorMessage);
+    }
+}
+
+
 // getMax - this function will take an array of integers and find
 // the max value in that array.
 //
@@ -41,18 +84,16 @@ function getMin(arr) {
 }
 
 // testing out getMax()
-console.log("Tests for getMax():");
-console.log(getMax([1,2,3]) + " == 3");
-console.log(getMax([5,2,4]) + " == 5");
-console.log(getMax([-1,-2,-3]) + " == -1");
-console.log(getMax([10]) + " == 10");
-console.log(getMax([]) + " == undefined");
+assert(getMax([1,2,3]) == 3, "using getMax");
+assert(getMax([5,2,4]) == 5, "using getMax");
+assert(getMax([-1,-2,-3]) == -1, "using getMax");
+assert(getMax([10]) == 10);
+assert(getMax([]) == undefined);
 
 // testing out getMin()
-console.log("Tests for getMin():");
-console.log(getMin([1,2,3]) + " == 1");
-console.log(getMin([5,2,4]) + " == 2");
-console.log(getMin([-1,-2,-3]) + " == -3");
-console.log(getMin([10]) + " == 10");
-console.log(getMin([]) + " == undefined");
+assert(getMin([1,2,3]) == 1);
+assert(getMin([5,2,4]) == 2);
+assert(getMin([-1,-2,-3]) == -3);
+assert(getMin([10]) == 10);
+assert(getMin([]) == undefined);
 
