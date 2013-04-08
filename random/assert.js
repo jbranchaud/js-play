e2d3adcb5ba9e1e546d57729ab1b4a904999fec4
jbranchaud <jbranchaud@gmail.com>
2013-04-07 20:41:50 -0500

    Adding a new function, assertFalse(), that holds when the given expression is false.

--- random/assert.js
+++ random/assert.js
@@ -13,7 +13,7 @@
     function Assert() {};
 
     /*
-     * assertTrue: expression -> void
+     * assertTrue: expression msg -> void
      * given some expression, this function will determine if that
      * expression is true or false. If it is true, then the function will
      * return normally because the assertion has held. However, if the
@@ -27,7 +27,7 @@
             message = "";
         }
         
-        // check the expression and throw an error if it is false
+        // check the expression and throw an error if it is not true
         if( expression != true) {
             var errorMessage = "Assert.assertTrue: assertion violated";
             if(message != "") {
@@ -36,6 +36,33 @@
             throw new Error(errorMessage);
         }
         
+        return;
+    };
+
+    /*
+     * assertFalse: expression msg -> void
+     * given some expression, this function will determine if that
+     * expression is true or false. If it is false, then the function will
+     * return normally because the assertion has held. However, if the
+     * expression is true, then the function will throw an error for an
+     * assertion violation.
+     */
+    Assert.prototype.assertFalse = function( expression, msg ) {
+
+        var message = msg;
+        if( message == null || typeof message === 'undefined' ) {
+            message = "";
+        }
+
+        // check the expression and throw an error if it is not false
+        if( expression != false ) {
+            var errorMessage = "Assert.assertFalse: assertion violated";
+            if(message != "") {
+                errorMessage = errorMessage + " - " + message;
+            }
+            throw new Error(errorMessage);
+        }
+
         return;
     };
 
